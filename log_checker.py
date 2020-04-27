5f1826454e668d0dd6b6d5ceceec6654d721dc3e
Alexander Lekontsev <sashalekoncev@gmail.com>
2020-04-27 19:40:12 +0500

    Added coroutine for message formatting, traceback now included

--- log_checker.py
+++ log_checker.py
@@ -1,24 +1,44 @@
-import time
+import asyncio
 
 
 class LogChecker(object):
 
-    def __init__(self, file_name, timeout=0.5):
+    def __init__(self, file_name, bot, timeout=0.5, search_string='Internal Server Error'):
+        self.mes = []
+        self.bot = bot
         self.timeout = timeout
         self.file_name = file_name
+        self.search_string = search_string
 
-    def process(self):
+    async def process(self):
         file = open(self.file_name)
-        for item in self.follow(file):
-            if "Internal Server Error" in item:
-                return item
-        return None
+        new_lines = self.follow(file, self.filter_on_search_string())
+        async for item in new_lines:
+            self.bot.notify(item)
+            self.mes.clear()
 
-    def follow(self, log_file):
+    def filter_on_search_string(self):
+        while True:
+            line = (yield)
+            self.mes.append(line)
+
+    def traceback_was_added(self):
+        if len(self.mes) > 2:
+            # 4 spaces and not '\t'
+            no_space_after = not self.mes[-1].startswith(' ')
+            four_spaces_before = self.mes[-2].startswith('    ')
+            search_string_in_title = self.search_string in self.mes[0]
+            return search_string_in_title and four_spaces_before and no_space_after
+
+    async def follow(self, log_file, a):
         log_file.seek(0, 2)
+        # init coroutine
+        next(a)
         while True:
             line = log_file.readline()
             if not line:
-                time.sleep(self.timeout)
+                await asyncio.sleep(self.timeout)
                 continue
-            yield line
+            a.send(line)
+            if self.traceback_was_added():
+                yield "".join(self.mes)
--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from log_checker import LogChecker
 from bot import LogFileBot
 from settings import *
 
-import time
+import asyncio
 
 
 def main():
     bot = LogFileBot(BOT_TOKEN, CHAT_ID, PROXY_URL)
-    log_checker = LogChecker(LOG_FILE_PATH)
-    while True:
-        message = log_checker.process()
-        if message:
-            bot.notify(message)
-        time.sleep(0.5)
+    log_checker = LogChecker(LOG_FILE_PATH, bot)
+    loop = asyncio.get_event_loop()
+    loop.create_task(log_checker.process())
+    loop.run_forever()
+    loop.close()
 
 
 if __name__ == '__main__':
